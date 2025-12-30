e8f66b548bdc41ec18bb6def3b3d52122520c5ac
Karthigaiselvam-R-official <karthigaiselvamr.cs2022@gmail.com>
2025-12-30 09:24:39 +0530

    Core: Add application entry point and logic

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QIcon>
+#include <QPixmap>
+#include <QDebug>
+#include "src/FanController.h"
+#include "src/SystemStatsMonitor.h"
+#include "src/AuraController.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    // --- DEBUG CHECK ---
+    QPixmap testLoad(":/ui/app_icon.png");
+    if (testLoad.isNull()) {
+        qCritical() << "ERROR: Image failed to load! Check file path and re-run cmake.";
+    } else {
+        qInfo() << "SUCCESS: Image loaded. Size:" << testLoad.size();
+        app.setWindowIcon(QIcon(testLoad));
+    }
+    // -------------------
+
+    qmlRegisterType<FanController>("AsusTufFanControl", 1, 0, "FanController");
+    qmlRegisterType<SystemStatsMonitor>("AsusTufFanControl", 1, 0, "SystemStatsMonitor");
+    qmlRegisterType<AuraController>("AsusTufFanControl", 1, 0, "AuraController");
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/ui/Main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    
+    engine.load(url);
+
+    return app.exec();
+}
